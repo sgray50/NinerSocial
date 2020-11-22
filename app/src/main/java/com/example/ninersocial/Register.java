f36104402a064dd7f2807172bd1b448cca3dcddd
shareace <sgray50@uncc.edu>
2020-11-22 15:47:43 -0500

    adding more authentications

--- app/src/main/java/com/example/ninersocial/Register.java
+++ app/src/main/java/com/example/ninersocial/Register.java
@@ -1,14 +1,88 @@
 package com.example.ninersocial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
+
+public class Register<FireBaseAuth> extends AppCompatActivity {
+    EditText nFullName, nEmail, nPassword;
+    Button nRegisterBtn;
+    TextView nLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nFullName = findViewById(R.id.fullName);
+        nEmail = findViewById(R.id.Email);
+        nPassword = findViewById(R.id.password);
+        nRegisterBtn = findViewById(R.id.registerBtn);
+        nLoginBtn = findViewById(R.id.loginBtn);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        nRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = nEmail.getText().toString().trim();
+                String password = nPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    nEmail.setError("Email is required.");
+                    return;
+
+                }
+                if (TextUtils.isEmpty(password)) {
+                    nPassword.setError("Password is required.");
+                    return;
+                }
+                if (password.length() < 8) {
+                    nPassword.setError("Password must be at least 8 characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
+            }
+        });
     }
 }

4060d98efbee4e7e09bf518599e75f2796239125
keye007 <keyeadewumi@gmail.com>
2020-12-16 02:21:50 -0500

    Update

--- app/src/main/java/com/example/ninersocial/Profile.java
+++ app/src/main/java/com/example/ninersocial/Profile.java
@@ -1,10 +1,13 @@
 package com.example.ninersocial;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
 
@@ -15,5 +18,11 @@ public class Profile extends AppCompatActivity {
     }
     public void home(View view) {
         Toast.makeText(Profile.this, "Hi there", Toast.LENGTH_SHORT).show();
+    }
+
+    public void logOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }

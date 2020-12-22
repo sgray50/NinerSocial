5a9f7d6ae6c60d499a51466755e46b9fe4e3078a
Rotimi Adewumi <rotimiadewumi98@gmail.com>
2020-12-22 12:50:34 -0500

    commit

--- app/src/main/java/com/example/ninersocial/Preferences.java
+++ app/src/main/java/com/example/ninersocial/Preferences.java
@@ -3,31 +3,21 @@ package com.example.ninersocial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Profile extends AppCompatActivity {
+public class Preferences extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_preferences);
     }
-    public void home(View view) {
-        Toast.makeText(Profile.this, "Hi there", Toast.LENGTH_SHORT).show();
-    }
-
-    public void logOut(View view) {
+    public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
-    }
-
-    public void ShowPreferences(View view) {
-        startActivity(new Intent(getApplicationContext(),Preferences.class));
         finish();
     }
 }
